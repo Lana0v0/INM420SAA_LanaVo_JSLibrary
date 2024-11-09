2a0700730b0a4637e4d586b7c8b9258b0cc8f512
Lana Vo <hvo3@myseneca.ca>
2024-11-09 17:31:46 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const glide = new Glide('.glide', {
+//       type: 'carousel',
+//       startAt: 0,
+//       perView: 3, // Number of images visible at a time
+//       gap: 20, // Space between each slide
+//       autoplay: 3000, // Auto-plays every 3 seconds
+//       hoverpause: true,
+//       breakpoints: {
+//         800: {
+//           perView: 2, // Show 2 slides on medium screens
+//         },
+//         480: {
+//           perView: 1, // Show 1 slide on small screens
+//         }
+//       }
+//     });
+  
+//     glide.mount();
+// 
+
+// $('.grid').masonry({
+//     // options
+//     itemSelector: '.grid-item',
+//     columnWidth: 100
+//   });
